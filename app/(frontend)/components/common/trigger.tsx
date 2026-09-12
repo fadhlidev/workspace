001222f61b30bb60963c87acc688cf0c10f9781c
Fadhli Syaifullah <fadhlisyaifullah@proton.me>
2026-09-12 14:02:11 +0700

    refactor: add generic value support to Trigger component

--- app/(frontend)/components/common/trigger.tsx
+++ app/(frontend)/components/common/trigger.tsx
@@ -3,44 +3,45 @@
 import { useState, Fragment, ReactNode, MouseEvent } from "react";
 import { useToggle } from "react-use";
 
-interface TriggerProps {
-  children: ({
-    isOpen,
-    handleOpen,
-  }: {
-    isOpen?: boolean;
-    handleOpen: (event?: MouseEvent<HTMLElement>) => void;
-    handleClose: () => void;
-  }) => ReactNode;
-  dialog: ({
-    anchorEl,
-    open,
-    onClose,
-  }: {
-    anchorEl: HTMLElement | null;
-    open: boolean;
-    onClose: () => void;
-  }) => ReactNode;
+interface TriggerChildrenProps<T> {
+  isOpen?: boolean;
+  handleOpen: (event?: MouseEvent<HTMLElement>, value?: T) => void;
+  handleClose: () => void;
 }
 
-export function Trigger({ children, dialog }: TriggerProps) {
+interface TriggerContentProps<T> {
+  value?: T;
+  open: boolean;
+  anchorEl: HTMLElement | null;
+  onClose: () => void;
+}
+
+interface TriggerProps<T> {
+  children: (props: TriggerChildrenProps<T>) => ReactNode;
+  content: (props: TriggerContentProps<T>) => ReactNode;
+}
+
+export function Trigger<T>({ children, content }: TriggerProps<T>) {
+  const [value, setValue] = useState<T | undefined>();
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const [open, toggleOpen] = useToggle(false);
 
-  function handleOpen(event?: MouseEvent<HTMLElement>) {
+  function handleOpen(event?: MouseEvent<HTMLElement>, value?: T) {
     if (event) setAnchorEl(event.currentTarget);
     toggleOpen(true);
+    setValue(value);
   }
 
   function handleClose() {
     setAnchorEl(null);
     toggleOpen(false);
+    setValue(undefined);
   }
 
   return (
     <Fragment>
       {children({ isOpen: open, handleOpen, handleClose })}
-      {dialog({ anchorEl, open, onClose: handleClose })}
+      {content({ open, value, anchorEl, onClose: handleClose })}
     </Fragment>
   );
 }
